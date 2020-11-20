a0e509b33ab98883ce2a6b3339fd6f4c0753cfbf
John William Becker Jr <Johnwilliambecker@gmail.com>
2020-11-20 11:37:10 -0500

    Slider update

--- src/components/player.js
+++ src/components/player.js
@@ -22,6 +22,13 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
         const duration = e.target.duration;
         setsongInfo({...songInfo, currentTime: current, duration });
     };
+
+    const getTime = (time) => {
+        return(
+            Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60 )).slice(-2)
+        )
+    }
+
     //State 
     const [songInfo, setsongInfo] = useState({
         currentTime: null, 
@@ -30,16 +37,26 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
     return (
         <div className = "player">
             <div className = "time-control">
-            <p>{ songInfo.currentTime }</p>
-                <input type = "range"/>
-                <p>End Time </p>
+            <p>{ getTime(songInfo.currentTime) }</p>
+                <input 
+                min= {0} 
+                max = {songInfo.duration} 
+                value = {songInfo.currentTime} 
+                type = "range"/>
+                
+                <p>{ getTime(songInfo.duration) }</p>
             </div>
             <div className = "play-control">
                 <FontAwesomeIcon className ="skip-back" size = "2x" icon = {faAngleLeft} />
                 <FontAwesomeIcon onClick = { playSongHandler } className ="play" size = "2x" icon = {faPlay} />
                 <FontAwesomeIcon className ="skip-forward" size = "2x" icon = {faAngleRight} />
             </div>
-            <audio onTimeUpdate = {timeUpdateHandler} ref = {audioRef} src = { currentSong.audio }></audio>
+            <audio 
+            onTimeUpdate = { timeUpdateHandler } 
+            ref = { audioRef }  
+            src = { currentSong.audio }
+            onLoadedMetadata = { currentSong }>     
+            </audio>
         </div>
     );
 };
